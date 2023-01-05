f517268ceb610d894761ec9de8b29f19b831b599
Explicit12 <explicit1723@gmail.com>
2023-01-06 00:55:26 +0200

    added header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { RouterLink, useRoute } from "vue-router";
+
+  const route = useRoute();
+</script>
+
+<template>
+  <header class="bg-green-600">
+    <nav class="my-0 mx-auto max-w-screen-2xl py-6 px-4">
+      <ul class="flex gap-6">
+        <li
+          :class="{ underline: route.name === 'home' }"
+          class="font-sans text-2xl font-medium text-white hover:cursor-pointer hover:underline"
+        >
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li
+          :class="{ underline: route.name === 'map' }"
+          class="font-sans text-2xl font-medium text-white hover:cursor-pointer hover:underline"
+        >
+          <RouterLink to="/">Map</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
