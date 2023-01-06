2896aead5590b91d0490fc4a8951a66221d10275
Explicit12 <explicit1723@gmail.com>
2023-01-06 11:19:43 +0200

    added filter by post title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <script setup>
-  import { onBeforeMount, ref } from "vue";
+  import { onBeforeMount, ref, computed, watch } from "vue";
   import { getPosts, getPostsComments } from "@/api/jsonplaceholder.js";
-  import { useRoute } from "vue-router";
+  import { useRoute, useRouter } from "vue-router";
 
   import PostsList from "@/components/PostsList.vue";
   import ThePagination from "@/components/ThePagination.vue";
 
   const posts = ref([]);
   const route = useRoute();
+  const router = useRouter();
+  const filterText = ref("");
+
+  const filteredPosts = computed(() => {
+    return posts.value.filter((post) =>
+      post.title.startsWith(filterText.value),
+    );
+  });
+
+  watch(filteredPosts, () => {
+    router.replace({ name: route.name, params: { page: 1 } });
+  });
 
   async function getPostsData() {
     const resPosts = await getPosts();
@@ -29,18 +41,26 @@
 <template>
   <main class="my-0 mx-auto max-w-screen-2xl py-12 px-4">
     <h1
-      class="line max-w-xl font-sans text-5xl font-bold leading-snug text-gray-900"
+      class="line mb-6 max-w-xl font-sans text-5xl font-bold leading-snug text-gray-900"
     >
       The most popular posts from JSONplaceholder!
     </h1>
+    <form>
+      <input
+        class="rounded-md border-2 border-gray-600 bg-gray-200 py-4 px-6 font-sans text-base font-normal text-gray-900 placeholder:text-gray-600 focus:border-gray-900"
+        type="text"
+        v-model="filterText"
+        placeholder="Search by title..."
+      />
+    </form>
     <p class="pt-6 font-sans text-base font-normal text-gray-900">
       Click on the post to see graph of commentators names
     </p>
     <PostsList
-      :posts="posts"
+      :posts="filteredPosts"
       :from="0 || Number(route.params.page) * 10 - 10"
       :to="Number(route.params.page) * 10"
     />
-    <ThePagination :pages="posts.length / 10" />
+    <ThePagination :pages="Math.round(filteredPosts.length / 10)" />
   </main>
 </template>
