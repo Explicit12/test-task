25602854299ad387ab8bc8a207d74b3897adbe43
Explicit12 <explicit1723@gmail.com>
2023-01-06 11:03:33 +0200

    added line clamp to prevent content jumping

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -15,10 +15,10 @@
   <article
     class="w-full rounded-lg border-2 border-gray-600 py-6 px-4 transition-shadow hover:cursor-pointer hover:shadow-md"
   >
-    <h2 class="font-sans text-lg font-bold text-gray-900">
+    <h2 class="font-sans text-lg font-bold text-gray-900 line-clamp-1">
       {{ post.title }}
     </h2>
-    <p class="font-sans text-base font-normal text-gray-900">
+    <p class="font-sans text-base font-normal text-gray-900 line-clamp-2">
       {{ post.body }}
     </p>
 
